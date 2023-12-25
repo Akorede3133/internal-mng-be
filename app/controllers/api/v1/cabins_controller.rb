5a9e5984839683a7ff432ad4b45e29b58adb6f63
akorede <emiolasaheed1@gmail.com>
2023-12-25 11:13:29 +0100

    Create index action for cabins

--- app/controllers/api/v1/cabins_controller.rb
+++ app/controllers/api/v1/cabins_controller.rb
@@ -1,3 +1,6 @@
-class CabinsController < ApplicationControll
-  
+class Api::V1::CabinsController < ApplicationController
+  def index
+    @cabins = Cabin.all
+    render json: @cabins, except: [:updated_at, :created_at]
+  end
 end
